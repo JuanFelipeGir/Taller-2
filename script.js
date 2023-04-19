3f840990f15e7a7d916e0640ebac49884d5fde40
JuanFelipeGir <juan.giraldo11@correo.tdea.edu.co>
2023-04-18 22:09:51 -0500

    Se subio bootstrap

--- script.js
+++ script.js
@@ -2,29 +2,39 @@ let productos = [];
 
 function agregarProducto() {
   let producto = document.getElementById('producto').value;
-  let cantidad = document.getElementById('cantidad').value;
+  let cantidad = parseInt(document.getElementById('cantidad').value);
+  let valor = parseInt(document.getElementById('valor').value);
 
   let nuevoProducto = {
     nombre: producto,
-    cantidad: cantidad
+    cantidad: cantidad,
+    valor: valor
   };
 
   productos.push(nuevoProducto);
-
-  document.getElementById('producto').value = '';
-  document.getElementById('cantidad').value = '';
-
   mostrarProductos();
 }
-function calcularTotal() {
-    let total = 0;
-  
-    for (let i = 0; i < productos.length; i++) {
-      let producto = productos[i];
-      total += producto.cantidad * 10; // Aquí asumimos que cada producto cuesta $10
-    }
-  
-    let pTotal = document.getElementById('total');
-    pTotal.innerText = 'Total a pagar: $' + total;
+
+function mostrarProductos() {
+  let listaProductos = document.getElementById('lista-productos');
+  listaProductos.innerHTML = '';
+
+  for (let i = 0; i < productos.length; i++) {
+    let producto = productos[i];
+    let li = document.createElement('li');
+    li.textContent = producto.nombre + ' - ' + producto.cantidad + ' - $' + producto.valor;
+    listaProductos.appendChild(li);
   }
-  
+}
+
+function calcularTotal() {
+  let total = 0;
+
+  for (let i = 0; i < productos.length; i++) {
+    let producto = productos[i];
+    total += producto.cantidad * producto.valor;
+  }
+
+  let divTotal = document.getElementById('total');
+  divTotal.innerHTML = 'Total a Pagar: $' + total;
+}
